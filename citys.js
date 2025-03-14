ea9640cc1ec90829e0848c554e575c9a70f71607
Himanshujays29 <himanshujayswal2006@gmail.com>
2025-03-14 19:27:48 +0530

    final commit

---
+++ citys.js
@@ -0,0 +1,426 @@
+cities = [
+    {
+      country: "India",
+      name: "Alirajpur",
+      lat: "22.3122",
+      lng: "74.3555",
+    },
+    {
+      country: "India",
+      name: "Anuppur",
+      lat: "23.1035",
+      lng: "81.6953",
+    },
+    {
+      country: "India",
+      name: "Ashoknagar",
+      lat: "24.5764",
+      lng: "77.7304",
+    },
+    {
+      country: "India",
+      name: "Balaghat",
+      lat: "21.8167",
+      lng: "80.1833",
+    },
+    {
+      country: "India",
+      name: "Barwani",
+      lat: "22.0333",
+      lng: "74.9",
+    },
+    {
+      country: "India",
+      name: "Betul",
+      lat: "21.9210",
+      lng: "77.9047",
+    },
+    {
+      country: "India",
+      name: "Bhind",
+      lat: "26.5667",
+      lng: "78.7873",
+    },
+    {
+      country: "India",
+      name: "Bhopal",
+      lat: "23.2599",
+      lng: "77.4126",
+    },
+    {
+      country: "India",
+      name: "Burhanpur",
+      lat: "21.3074",
+      lng: "76.2305",
+    },
+    {
+      country: "India",
+      name: "Chhatarpur",
+      lat: "24.9177",
+      lng: "79.5880",
+    },
+    {
+      country: "India",
+      name: "Chhindwara",
+      lat: "22.0574",
+      lng: "78.9382",
+    },
+    {
+      country: "India",
+      name: "Damoh",
+      lat: "23.8360",
+      lng: "79.4502",
+    },
+    {
+      country: "India",
+      name: "Datia",
+      lat: "25.6725",
+      lng: "78.4598",
+    },
+    {
+      country: "India",
+      name: "Dewas",
+      lat: "22.9676",
+      lng: "76.0534",
+    },
+    {
+      country: "India",
+      name: "Dhar",
+      lat: "22.6013",
+      lng: "75.3020",
+    },
+    {
+      country: "India",
+      name: "Dindori",
+      lat: "22.9435",
+      lng: "81.0748",
+    },
+    {
+      country: "India",
+      name: "Guna",
+      lat: "24.6477",
+      lng: "77.3119",
+    },
+    {
+      country: "India",
+      name: "Gwalior",
+      lat: "26.2183",
+      lng: "78.1828",
+    },
+    {
+      country: "India",
+      name: "Harda",
+      lat: "22.3387",
+      lng: "77.0957",
+    },
+    {
+      country: "India",
+      name: "Hoshangabad",
+      lat: "22.7500",
+      lng: "77.7167",
+    },
+    {
+      country: "India",
+      name: "Indore",
+      lat: "22.7196",
+      lng: "75.8577",
+    },
+    {
+      country: "India",
+      name: "Jabalpur",
+      lat: "23.1815",
+      lng: "79.9864",
+    },
+    {
+      country: "India",
+      name: "Jhabua",
+      lat: "22.7675",
+      lng: "74.5906",
+    },
+    {
+      country: "India",
+      name: "Katni",
+      lat: "23.8378",
+      lng: "80.3870",
+    },
+    {
+      country: "India",
+      name: "Khandwa",
+      lat: "21.8335",
+      lng: "76.3495",
+    },
+    {
+      country: "India",
+      name: "Khargone",
+      lat: "21.8257",
+      lng: "75.6151",
+    },
+    {
+      country: "India",
+      name: "Mandla",
+      lat: "22.5995",
+      lng: "80.3714",
+    },
+    {
+      country: "India",
+      name: "Mandsaur",
+      lat: "24.0735",
+      lng: "75.0690",
+    },
+    {
+      country: "India",
+      name: "Morena",
+      lat: "26.5028",
+      lng: "78.0014",
+    },
+    {
+      country: "India",
+      name: "Narsinghpur",
+      lat: "22.9500",
+      lng: "79.1833",
+    },
+    {
+      country: "India",
+      name: "Neemuch",
+      lat: "24.4720",
+      lng: "74.8728",
+    },
+    {
+      country: "India",
+      name: "Panna",
+      lat: "24.7146",
+      lng: "80.1875",
+    },
+    {
+      country: "India",
+      name: "Raisen",
+      lat: "23.3306",
+      lng: "77.7950",
+    },
+    {
+      country: "India",
+      name: "Rajgarh",
+      lat: "23.8577",
+      lng: "76.7274",
+    },
+    {
+      country: "India",
+      name: "Ratlam",
+      lat: "23.3342",
+      lng: "75.0367",
+    },
+    {
+      country: "India",
+      name: "Rewa",
+      lat: "24.5362",
+      lng: "81.3041",
+    },
+    {
+      country: "India",
+      name: "Sagar",
+      lat: "23.8388",
+      lng: "78.7378",
+    },
+    {
+      country: "India",
+      name: "Satna",
+      lat: "24.6005",
+      lng: "80.8322",
+    },
+    {
+      country: "India",
+      name: "Sehore",
+      lat: "23.2000",
+      lng: "77.0833",
+    },
+    {
+      country: "India",
+      name: "Seoni",
+      lat: "22.0856",
+      lng: "79.5504",
+    },
+    {
+      country: "India",
+      name: "Shahdol",
+      lat: "23.3000",
+      lng: "81.3500",
+    },
+    {
+      country: "India",
+      name: "Shajapur",
+      lat: "23.4260",
+      lng: "76.2778",
+    },
+    {
+      country: "India",
+      name: "Sheopur",
+      lat: "25.6668",
+      lng: "76.6961",
+    },
+    {
+      country: "India",
+      name: "Shivpuri",
+      lat: "25.4236",
+      lng: "77.6644",
+    },
+    {
+      country: "India",
+      name: "Sidhi",
+      lat: "24.3956",
+      lng: "81.8825",
+    },
+    {
+      country: "India",
+      name: "Singrauli",
+      lat: "24.1997",
+      lng: "82.6754",
+    },
+    {
+      country: "India",
+      name: "Tikamgarh",
+      lat: "24.7500",
+      lng: "78.8333",
+    },
+    {
+      country: "India",
+      name: "Ujjain",
+      lat: "23.1793",
+      lng: "75.7849",
+    },
+    {
+      country: "India",
+      name: "Umaria",
+      lat: "23.5245",
+      lng: "80.8372",
+    },
+    {
+      country: "India",
+      name: "Vidisha",
+      lat: "23.5236",
+      lng: "77.8103",
+    },
+    {
+        country: "India",
+        name: "Mumbai",
+        lat: "19.0760",
+        lng: "72.8777"
+    },
+    {
+        country: "India",
+        name: "Delhi",
+        lat: "28.7041",
+        lng: "77.1025"
+    },
+    {
+        country: "India",
+        name: "Kolkata",
+        lat: "22.5726",
+        lng: "88.3639"
+    },
+    {
+        country: "India",
+        name: "Chennai",
+        lat: "13.0827",
+        lng: "80.2707"
+    },
+    {
+        country: "India",
+        name: "Bangalore",
+        lat: "12.9716",
+        lng: "77.5946"
+    },
+    {
+        country: "India",
+        name: "Hyderabad",
+        lat: "17.3850",
+        lng: "78.4867"
+    },
+    {
+        country: "India",
+        name: "Ahmedabad",
+        lat: "23.0225",
+        lng: "72.5714"
+    },
+    {
+        country: "India",
+        name: "Pune",
+        lat: "18.5204",
+        lng: "73.8567"
+    },
+    {
+        country: "India",
+        name: "Surat",
+        lat: "21.1702",
+        lng: "72.8311"
+    },
+    {
+        country: "India",
+        name: "Jaipur",
+        lat: "26.9124",
+        lng: "75.7873"
+    },
+    {
+        country: "India",
+        name: "Lucknow",
+        lat: "26.8467",
+        lng: "80.9462"
+    },
+    {
+        country: "India",
+        name: "Kanpur",
+        lat: "26.4499",
+        lng: "80.3319"
+    },
+    {
+        country: "India",
+        name: "Nagpur",
+        lat: "21.1458",
+        lng: "79.0882"
+    },
+    {
+        country: "India",
+        name: "Patna",
+        lat: "25.5941",
+        lng: "85.1376"
+    },
+    {
+        country: "India",
+        name: "Ranchi",
+        lat: "23.3441",
+        lng: "85.3096"
+    },
+    {
+        country: "India",
+        name: "Bhubaneswar",
+        lat: "20.2961",
+        lng: "85.8245"
+    },
+    {
+        country: "India",
+        name: "Guwahati",
+        lat: "26.1445",
+        lng: "91.7362"
+    },
+    {
+        country: "India",
+        name: "Chandigarh",
+        lat: "30.7333",
+        lng: "76.7794"
+    },
+    {
+        country: "India",
+        name: "Thiruvananthapuram",
+        lat: "8.5241",
+        lng: "76.9366"
+    },
+    {
+        country: "India",
+        name: "Kochi",
+        lat: "9.9312",
+        lng: "76.2673"
+    }
+
+
+  
+  
+];
